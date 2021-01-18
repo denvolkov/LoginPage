7bf56d108d53a55514baa1e2f74e16fbe924edf7
denvolkov <denvolkov@inbox.ru>
2021-01-18 15:37:06 +0300

    Initial Commit

---
+++ LoginPage/LoginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageApp.swift
+//  LoginPage
+//
+//  Created by test on 18.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
